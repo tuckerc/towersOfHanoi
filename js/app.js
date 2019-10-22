5435f78546028c43697c59d8be458307e8d63f90
Chase Tucker <tuck8106@gmail.com>
2019-10-22 14:30:57 -0700

    committing before pulling in working solution

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ function Pole(id){
       //!!!!!!!!!!!!!!!!!!!!!
       //remove later !!!!!!!!pole1
       //!!!!!!!!!!!!!!!!!!!!!
-      render();
+      // render();
     }
     else if(poles[poleNumber].isSmaller()) {
       poles[poleNumber].donuts.push(poles[fromPole].donuts[poles[fromPole].donuts.length-1]);
@@ -81,6 +81,77 @@ function render() {
   console.log(poles);
   console.log(fromPole);
   console.log(moves);
+  
+  //clear the poles
+  var pole1 = document.getElementById('post1');
+  // console.log(pole1.children);
+  // for(var i = 0; i < pole1.children.length; i++) {
+  //   console.log(pole1.children[i]);
+  //   pole1.removeChild(pole1.children[i]);
+  // }
+  pole1.innerHTML = '';
+  var pole2= document.getElementById('post2');
+  // for(var i = 0; i < pole2.children.length; i++) {
+  //   pole2.removeChild(pole2.children[i]);
+  // }
+  pole2.innerHTML = '';
+  var pole3= document.getElementById('post3');
+  // for(var i = 0; i < pole3.children.length; i++) {
+  //   pole3.removeChild(pole3.children[i]);
+  // }
+  pole3.innerHTML = '';
+  // fill the poles
+  for(var i = 0; i < poles[0].donuts.length; i++) {
+    var pole1 = document.getElementById('post1');
+    var newDonut = document.createElement('div');
+    var donutSize = poles[0].donuts[i].size;
+    if(donutSize === 1) {
+      newDonut.classList.add('donutOne');
+      pole1.appendChild(newDonut);
+    }
+    else if (donutSize === 2) {
+      newDonut.classList.add('donutTwo');
+      pole1.appendChild(newDonut);
+    }
+    else if (donutSize === 3) {
+      newDonut.classList.add('donutThree');
+      pole1.appendChild(newDonut);
+    }
+  }
+  for(var i = 0; i < poles[1].donuts.length; i++) {
+    var pole2= document.getElementById('post2');
+    var newDonut = document.createElement('div');
+    var donutSize = poles[1].donuts[i].size;
+    if(donutSize === 1) {
+      newDonut.classList.add('donutOne');
+      pole2.appendChild(newDonut);
+    }
+    else if (donutSize === 2) {
+      newDonut.classList.add('donutTwo');
+      pole2.appendChild(newDonut);
+    }
+    else if (donutSize === 3) {
+      newDonut.classList.add('donutThree');
+      pole2.appendChild(newDonut);
+    }
+  }
+  for(var i = 0; i < poles[2].donuts.length; i++) {
+    var pole3 = document.getElementById('post2');
+    var newDonut = document.createElement('div');
+    var donutSize = poles[0].donuts[i].size;
+    if(donutSize === 1) {
+      newDonut.classList.add('donutOne');
+      pole3.appendChild(newDonut);
+    }
+    else if (donutSize === 2) {
+      newDonut.classList.add('donutTwo');
+      pole3.appendChild(newDonut);
+    }
+    else if (donutSize === 3) {
+      newDonut.classList.add('donutThree');
+      pole3.appendChild(newDonut);
+    }
+  }
 }
 
 // instantiate a new pole1
@@ -109,20 +180,20 @@ document.getElementById('instructions').addEventListener('click', function() {
 
 render();
 
-var post1El = document.getElementById('post1');
+// var post1El = document.getElementById('post1');
 
-var donutThree = document.createElement('div')
-donutThree.classList.add('donutThree');
-donutThree.textContent = "BIG";
-post1El.appendChild(donutThree);
+// var donutThree = document.createElement('div')
+// donutThree.classList.add('donutThree');
+// donutThree.textContent = "BIG";
+// post1El.appendChild(donutThree);
 
-var donutTwo = document.createElement('div')
-donutTwo.classList.add('donutTwo');
-donutTwo.textContent = "MEDIUM";
-post1El.appendChild(donutTwo);
+// var donutTwo = document.createElement('div')
+// donutTwo.classList.add('donutTwo');
+// donutTwo.textContent = "MEDIUM";
+// post1El.appendChild(donutTwo);
 
-var donutOne = document.createElement('div')
-donutOne.classList.add('donutOne');
-donutOne.textContent = "SMALL";
-post1El.appendChild(donutOne);
+// var donutOne = document.createElement('div')
+// donutOne.classList.add('donutOne');
+// donutOne.textContent = "SMALL";
+// post1El.appendChild(donutOne);
 
