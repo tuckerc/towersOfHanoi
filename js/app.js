fb5f94da72e9313973a87d0e81686af6346e5ea1
RAN VAKNIN <ranvaknin0@gmail.com>
2019-10-22 09:59:21 -0700

    dont merge this

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // new variable to store chosen pole's id
 var fromPole = null;
-
+// counter for moves
+var moves = 0;
 //constructor function for donuts
 function Donut(size){
   this.size = size;
@@ -11,11 +12,6 @@ function Pole(id){
   this.donuts =[];
   // comparison function to determine if legal move
   this.isSmaller = function (){
-
-    if(this.donuts.length === 0){
-      return true;
-    } else if (this.pole[fromPole].donuts.length === 0){
-
     if (poles[fromPole].donuts.length === 0){
       fromPole = null;
       alert('no donouts to move');
@@ -27,41 +23,41 @@ function Pole(id){
     else if (this.donuts[this.donuts.length-1].size > poles[fromPole].donuts[poles[fromPole].donuts.length-1].size) {
       return true;
     } else {
+      fromPole = null;
+      alert('you can\'t stack a big donut on a smaller donut');
       return false;
     }
   };
   // function to handle donut moves
   this.move = function () {
+    
+    var poleNumber = Number(this.id[this.id.length - 1] - 1);
     if (fromPole === null){
-      fromPole = this.id;
+      fromPole = poleNumber;
       //!!!!!!!!!!!!!!!!!!!!!
-      //remove later !!!!!!!!
+      //remove later !!!!!!!!pole1
       //!!!!!!!!!!!!!!!!!!!!!
-      render();
-    }else if(this.isSmaller()) {
-      this.donuts.push(poles[fromPole].donuts[poles[fromPole].donuts.length-1]);
-      poles[fromPole].donuts.pop();
       render();
     }
-  }
+    else if(poles[poleNumber].isSmaller()) {
+      poles[poleNumber].donuts.push(poles[fromPole].donuts[poles[fromPole].donuts.length-1]);
+      poles[fromPole].donuts.pop();
+      fromPole = null;
+      moves++;
+      render();
+    }
+  };
 }
-}
-
-
-
-
 // checks to see if we have a winner
 function isAWinner() {
   if(poles[1].donuts.length === 3 || poles[2].donuts.length === 3) {
     winnerWinner();
   }
 }
-
 // this is where we putt our animation for the winner
 function winnerWinner() {
   alert(`Congratulations! You completed Towers Of Hanoi level ${poles.length} in ${moves} moves!`);
 }
-
 function reset() {
   for (var i = 0; i < poles.length; i++) {
     poles[i].donuts = [];
@@ -73,7 +69,6 @@ function reset() {
   fromPole = null;
   render();
 }
-
 function render() {
   document.getElementById('moves').textContent = `Moves: ${moves}`;
   isAWinner();
@@ -81,21 +76,17 @@ function render() {
   console.log(fromPole);
   console.log(moves);
 }
-
 // instantiate a new pole1
 var pole0 = new Pole(0);
 //populate the pole with 3 donuts at the beginning of page.
 pole0.donuts.push(new Donut(3));
 pole0.donuts.push(new Donut(2));
 pole0.donuts.push(new Donut(1));
-
 // instantiate a new poles 2 and 3/
 var pole1 = new Pole(1);
 var pole2 = new Pole(2);
 var poles = [pole0,pole1,pole2];
-
 // add pole event listeners
-
 document.getElementById('post1').addEventListener('click', poles[0].move);
 document.getElementById('post2').addEventListener('click', poles[1].move);
 document.getElementById('post3').addEventListener('click', poles[2].move);
@@ -106,5 +97,4 @@ document.getElementById('about').addEventListener('click', function() {
 document.getElementById('instructions').addEventListener('click', function() {
   document.location.assign('instructions.html');
 });
-
 render();
