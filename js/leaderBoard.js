98c74f722d667acc1e01c5895725313e2e0a2128
Chase Tucker <tuck8106@gmail.com>
2019-10-24 17:35:30 -0700

    leaderboard updated

--- js/leaderBoard.js
+++ js/leaderBoard.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// add event listeners to nav buttons
 document.getElementById('about').addEventListener('click', function() {
   document.location.assign('about.html');
 });
@@ -11,19 +12,67 @@ document.getElementById('index').addEventListener('click', function() {
   document.location.assign('index.html');
 });
 
+// function to render the leader board
 function displayBoard() {
-  var table = document.getElementById('leaderBoard');
+  
   if(localStorage.getItem('towersOfHanoi')) {
     var board = JSON.parse(localStorage.getItem('towersOfHanoi')).board;
-    for(var i = 0; i < 10; i++) {
-      var newTR = document.createElement('tr');
-      var name = document.createElement('td');
-      var moves = document.createElement('td');
-      name.textContent = board[i].name;
-      moves.textContent = board[i].moves;
-      newTR.appendChild(name);
-      newTR.appendChild(moves);
-      table.appendChild(newTR);
+    var counter = 0;
+    var index = 0;
+
+    // three donuts table
+    var table = document.getElementById('threeDonuts');
+    while(counter < 10 && index < board.length) {
+      if(board[index].donutCount === 3) {
+        var newTR = document.createElement('tr');
+        var name = document.createElement('td');
+        var moves = document.createElement('td');
+        name.textContent = board[index].name;
+        moves.textContent = board[index].moves;
+        newTR.appendChild(name);
+        newTR.appendChild(moves);
+        table.appendChild(newTR);
+        counter++;
+      }
+      index++;
+    }
+
+    // four donuts table
+    counter = 0;
+    index = 0;
+    table = document.getElementById('fourDonuts');
+    while(counter < 10 && index < board.length) {
+      if(board[index].donutCount === 4) {
+        var newTR = document.createElement('tr');
+        var name = document.createElement('td');
+        var moves = document.createElement('td');
+        name.textContent = board[index].name;
+        moves.textContent = board[index].moves;
+        newTR.appendChild(name);
+        newTR.appendChild(moves);
+        table.appendChild(newTR);
+        counter++;
+      }
+      index++;
+    }
+
+    // five donuts table
+    counter = 0;
+    index = 0;
+    table = document.getElementById('fiveDonuts');
+    while(counter < 10 && index < board.length) {
+      if(board[index].donutCount === 5) {
+        var newTR = document.createElement('tr');
+        var name = document.createElement('td');
+        var moves = document.createElement('td');
+        name.textContent = board[index].name;
+        moves.textContent = board[index].moves;
+        newTR.appendChild(name);
+        newTR.appendChild(moves);
+        table.appendChild(newTR);
+        counter++;
+      }
+      index++;
     }
   }
 }
